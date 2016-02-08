67b0a2827eb1fa199ed40936b07732954903f936
Yuki Sakata <doragon3.0920@gmail.com>
2016-02-08 21:47:11 +0900

    基礎のみ

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var label: UILabel!
+    
+    
+    var number: Int = 0
+    var number2: Int = 0
+    var oparation: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func select3(){
+        number = number*10 + 3
+        label.text = String(number)
+    }
+    
+    @IBAction func select4(){
+        number = number*10 + 4
+        label.text = String(number)
+    }
+    
+    
+    @IBAction func plus(){
+        //plusボタンを押した時に、labelに0が表示される
+        label.text = "0"
+        
+        oparation = 1
+        number2 = number
+        number = 0
     }
 
+    @IBAction func equal(){
+        if oparation == 1{
+            label.text = String(number + number2)
+        }
+    }
+    
 
 }
 
